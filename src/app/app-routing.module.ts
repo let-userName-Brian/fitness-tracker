946e284d64d70c58089ed3d27511e9bba42ea57b
Brian <brian.hardy67@gmail.com>
2022-02-23 17:12:33 -0500

    auth guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGaurd } from './auth/auth-guard';
 
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
@@ -10,11 +11,12 @@ const routes: Routes = [
   { path: '', component: WelcomeComponent},
   { path: 'signup', component: SignupComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'training', component: TrainingComponent}
+  { path: 'training', component: TrainingComponent, canActivate: [AuthGaurd]}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGaurd]
 })
 export class AppRoutingModule { }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,38 +2,46 @@ import { User } from "./user.model";
 import { AuthData } from "./auth-data.model";
 import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
+import { Router } from "@angular/router";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
   private user: User;
   authChange = new Subject<boolean>();
+  constructor(private router: Router) { }
 
-  registerUser( authData: AuthData){
+  registerUser(authData: AuthData) {
     this.user = {
       email: authData.email,
       userId: Math.round(Math.random() * 10000).toString()
     };
-    this.authChange.next(true);
+    this.authSuccessfully();
   }
 
-  login( authData: AuthData) {
+  login(authData: AuthData) {
     this.user = {
       email: authData.email,
       userId: Math.round(Math.random() * 10000).toString()
     };
-    this.authChange.next(true);
+    this.authSuccessfully();
   }
 
-  logout(){
+  logout() {
     this.user = null;
     this.authChange.next(false);
+    this.router.navigate(["/login"]);
   }
 
-  getUser(){
+  getUser() {
     return { ...this.user };
   }
 
-  isAuthenticated(){
+  isAuthenticated() {
     return this.user != null;
+  }
+
+  private authSuccessfully() {
+    this.authChange.next(true);
+    this.router.navigate(["/"]);
   }
 }
