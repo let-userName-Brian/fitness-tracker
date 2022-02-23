1d706dacbc4199fdb3568cee1a239401e280523a
let-userName-Brian <brian.hardy67@gmail.com>
2022-02-22 19:28:27 -0500

    init

---
+++ src/app/material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+
+@NgModule({
+  imports: [
+    // import all the modules that are required
+  ],
+  exports: [
+    // export all the modules that are required
+  ]
+  
+})
+
+export class MaterialModule {
+
+}
