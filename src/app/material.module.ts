477ad62cf5fa5401e11eb818be372e4c3416c2a0
let-userName-Brian <brian.hardy67@gmail.com>
2022-02-22 19:56:24 -0500

    basic componets/routes config

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from "@angular/core";
 
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+
 @NgModule({
   imports: [
     // import all the modules that are required
+    MatButtonModule,
+    MatIconModule
   ],
   exports: [
     // export all the modules that are required
+    MatButtonModule,
+    MatIconModule
   ]
-  
+
 })
 
 export class MaterialModule {
