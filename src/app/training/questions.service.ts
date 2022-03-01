166192463ff4e767c531deb97b7796767ffe0c0f
Brian <brian.hardy67@gmail.com>
2022-02-28 19:33:05 -0500

    modified switch/case to reflect data

--- src/app/training/questions.service.ts
+++ src/app/training/questions.service.ts
@@ -11,29 +11,31 @@ export class QuestionsService {
     this.databank = qc;
     switch (qc) {
       case "Flight Sargent":
-       return this.databank = "FS";
+        return this.databank = "FS";
       case "RFL - Patrol":
         return this.databank = "RFL";
       case "IEC/RFM":
         return this.databank = "RFM";
       case "Security Controller":
         return this.databank = "SC";
-      case "QC5":
-        return this.databank = "QC5";
+      case "Armorer":
+        return this.databank = "Armory";
+      case "Alarm Monitor":
+        return this.databank = "AM"
     }
   }
-/**
- * @description Filters the questions to a limit of 25 and randomizes the questions
- *  *possible refactor*
- */
-  filterQuestions(){
+  /**
+   * @description Filters the questions to a limit of 25 and randomizes the questions
+   *  *possible refactor*
+   */
+  filterQuestions() {
     let randomQuestions = [];
     let randomIndex = 0;
     let randomQuestion = null;
-    while(randomQuestions.length < 25){
+    while (randomQuestions.length < 25) {
       randomIndex = Math.floor(Math.random() * this.fetchedQuestions.length);
       randomQuestion = this.fetchedQuestions[randomIndex];
-      if(randomQuestions.indexOf(randomQuestion) === -1){
+      if (randomQuestions.indexOf(randomQuestion) === -1) {
         randomQuestions.push(randomQuestion);
       }
     }
@@ -44,8 +46,8 @@ export class QuestionsService {
     let params = this.databank;
     return this.http.get(
       `https://qc-database-aee15-default-rtdb.firebaseio.com/${params}/${params}.json`).subscribe((response: any) => {
-      this.fetchedQuestions = response;
-      this.filterQuestions();
-    });
+        this.fetchedQuestions = response;
+        this.filterQuestions();
+      });
   }
 }
