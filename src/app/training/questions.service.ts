086855a85c55e252c42b69d3201ac217e66719f0
Brian <brian.hardy67@gmail.com>
2022-03-03 12:30:56 -0500

    added DB routes for the new changes/migrations

--- src/app/training/questions.service.ts
+++ src/app/training/questions.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { ExerciseService } from "./exercise.service";
 
 @Injectable({ providedIn: "root" })
 export class QuestionsService {
@@ -7,7 +9,11 @@ export class QuestionsService {
   fetchedQuestions: any;
   newQuestion: any;
   editedQuestion: any;
-  constructor(private http: HttpClient) { }
+
+  verbalsCompleted: any; //holds all verbals and is used for the cards awaiting practical
+  allCompletedQCs: any; //holds all fully completed and is used for the data table
+
+  constructor(private http: HttpClient, private router: Router, private exerciseService: ExerciseService) { }
 
   getNameofQuestionBank(qc: string) {
     this.databank = qc;
@@ -77,6 +83,84 @@ export class QuestionsService {
       this.editedQuestion = response;
     });
   }
+
+  /**
+   * 
+   * @param score the score of the QC
+   * @param exam the exam to be completed
+   * @returns finishes the QC and moves it to the awaiting practical page
+   */
+  completedVerbal(score: number, exam: any) {
+    let id = exam.id;
+    return this.http.post(
+      `https://qc-database-aee15-default-rtdb.firebaseio.com/completedVerbals/${id}.json`, {
+      exam: exam,
+      score: score,
+      user: this.exerciseService.userName
+    })
+  }
+
+  /**
+   * 
+   * @param exam the exam to be deleted
+   * @returns removes the QC from the completed verbals so it doesnt show up as awaiting practical
+   */
+  deleteVerbalCompleted(exam: any) {
+    let id = exam.id;
+    return this.http.delete(
+      `https://qc-database-aee15-default-rtdb.firebaseio.com/completedVerbals/${id}.json`)
+  }
+
+  /**
+   * 
+   * @param score the score of the QC
+   * @param exam the actual exam
+   * @returns moves the qc to the completed screen and data table
+   */
+  completedPractical(score: number, exam: any) {
+    let id = exam.id;
+    return this.http.post(
+      `https://qc-database-aee15-default-rtdb.firebaseio.com/completedQCs/${id}.json`, {
+      exam: exam,
+      score: score,
+      user: this.exerciseService.userName
+    })
+  }
+
+  /**
+   * 
+   * @param exam the exam to be deleted
+   * @returns 
+   * needs a subscription to be used but deletes a full QC from the DB
+   */
+  deleteQC(exam:any){
+    let id = exam.id;
+    return this.http.delete(
+      `https://qc-database-aee15-default-rtdb.firebaseio.com/completedQCs/${id}.json`)
+  }
+
+
+  /**
+   * 
+   * @returns the completed verbals
+   * used for the cards on the verbal completed page
+   */
+  getVerbalCompleted() {
+    return this.http.get(
+      `https://qc-database-aee15-default-rtdb.firebaseio.com/completedVerbals.json`).subscribe((response: any) => {
+        this.verbalsCompleted = response;
+      });
+  }
+
+  /**
+   * 
+   * @returns all completed QC's
+   * used for the data table
+   */
+  getCompletedQCs() {
+    return this.http.get(
+      `https://qc-database-aee15-default-rtdb.firebaseio.com/completedQCs.json`).subscribe((response: any) => {
+        this.allCompletedQCs = response;
+      });
+  }
 }
-
-
