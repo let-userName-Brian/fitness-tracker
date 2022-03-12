45ec32af832e9640323d6f15b4b407a7ba91991b
Brian <brian.hardy67@gmail.com>
2022-03-12 14:54:21 -0500

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAsGVfFKtQM1hfOVEODUYUqBZrl-0NPjOM",
+    authDomain: "qc-database-aee15.firebaseapp.com",
+    projectId: "qc-database-aee15",
+    storageBucket: "qc-database-aee15.appspot.com",
+    messagingSenderId: "385415778283",
+    appId: "1:385415778283:web:37140b64ca571bc76153ae"
+  }
 };
